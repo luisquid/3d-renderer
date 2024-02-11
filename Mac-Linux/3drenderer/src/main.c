4a45cc5056decf0f6ab7343ce17b85673033801b
Squid <luis.bazan@unity3d.com>
2024-02-11 14:58:01 -0500

    Matrix transformations

--- Mac-Linux/3drenderer/src/main.c
+++ Mac-Linux/3drenderer/src/main.c
@@ -122,8 +122,23 @@ void update(void) {
     mesh.rotation.z += 0.01;
     mesh.scale.x += 0.002;
     mesh.scale.y += 0.001;
+    mesh.translation.x += 0.006;
+
+    mesh.translation.z = 5.0;
 
     mat4_t scale_matrix = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.z);
+    mat4_t translation_matrix = mat4_make_translation(mesh.translation.x, mesh.translation.y, mesh.translation.z);
+    mat4_t rotation_matrix_x = mat4_make_rotation_x(mesh.rotation.x);
+    mat4_t rotation_matrix_y = mat4_make_rotation_y(mesh.rotation.y);
+    mat4_t rotation_matrix_z = mat4_make_rotation_z(mesh.rotation.z);
+
+    // ORDER MATTERS!! Scale -> Rotate -> Translate
+    mat4_t world_matrix = mat4_identity();
+    world_matrix = mat4_mul_mat4(scale_matrix, world_matrix);
+    world_matrix = mat4_mul_mat4(rotation_matrix_x, world_matrix);
+    world_matrix = mat4_mul_mat4(rotation_matrix_y, world_matrix);
+    world_matrix = mat4_mul_mat4(rotation_matrix_z, world_matrix);
+    world_matrix = mat4_mul_mat4(translation_matrix, world_matrix);
 
     // Loop all triangle faces of our mesh
     int num_faces = array_length(mesh.faces);
@@ -141,15 +156,20 @@ void update(void) {
         for (int j = 0; j < 3; j++) {
             vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
+            transformed_vertex = mat4_mul_vec4(world_matrix, transformed_vertex);
             // USE A MATRIX
-            transformed_vertex = mat4_mul_ve4(scale_matrix, transformed_vertex);
+            // transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
+            // transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
+            // transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
+            // transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
+            // transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
 
             // transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
             // transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
             // transformed_vertex = vec3_rotate_z(transformed_vertex, mesh.rotation.z);
 
             // Translate the vertex away from the camera
-            transformed_vertex.z += 5;
+            // transformed_vertex.z += 5;
 
             transformed_vertices[j] = transformed_vertex;
         }
