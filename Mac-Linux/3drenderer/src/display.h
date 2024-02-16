eabfd049890868476978b66698cc2f46d723d875
Squid <luis.bazan@unity3d.com>
2024-02-16 00:27:36 -0500

    Texture Mapping is kind of done

--- Mac-Linux/3drenderer/src/display.h
+++ Mac-Linux/3drenderer/src/display.h
@@ -8,6 +8,20 @@
 #define FPS 60 
 #define FRAME_TARGET_TIME (1000 / FPS)
 
+enum cull_method{
+    CULL_NONE,
+    CULL_BACKFACE
+}cull_method;
+
+enum render_mode{
+    RENDER_WIRE, 
+    RENDER_WIRE_VERTEX, 
+    RENDER_FILL_TRIANGLE, 
+    RENDER_FILL_TRIANGLE_WIRE,
+    RENDER_TEXTURED,
+    RENDER_TEXTURED_WIRE
+}render_mode;
+
 extern SDL_Window* window;
 extern SDL_Renderer* renderer;
 extern uint32_t* color_buffer;
@@ -19,7 +33,6 @@ bool initialize_window(void);
 void draw_grid(void);
 void draw_pixel(int x, int y, uint32_t color);
 void draw_line(int x0, int y0, int x1, int y1, uint32_t color);
-void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
 void draw_rect(int x, int y, int width, int height, uint32_t color);
 void render_color_buffer(void); 
 void clear_color_buffer(uint32_t color);
--- Mac-Linux/3drenderer/src/main.c
+++ Mac-Linux/3drenderer/src/main.c
@@ -8,6 +8,8 @@
 #include "mesh.h"
 #include "matrix.h"
 #include "light.h"
+#include "texture.h"
+#include "triangle.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 // Array of triangles that should be rendered frame by frame
@@ -18,18 +20,6 @@ triangle_t* triangles_to_render = NULL;
 // Global variables for execution status and game loop
 ///////////////////////////////////////////////////////////////////////////////
 bool is_running = false;
-
-enum cull_method{
-    CULL_NONE,
-    CULL_BACKFACE
-}cull_method;
-
-enum render_mode{
-    RENDER_WIRE, 
-    RENDER_WIRE_VERTEX, 
-    RENDER_FILL_TRIANGLE, 
-    RENDER_FILL_TRIANGLE_WIRE
-}render_mode;
 
 int previous_frame_time = 0;
 
@@ -62,11 +52,15 @@ void setup(void) {
     float zfar = 100.0;
     proj_matrix = mat4_make_perspective(fov, aspect, znear, zfar);
 
+    mesh_texture = (uint32_t*) REDBRICK_TEXTURE;
+    texture_width = 64;
+    texture_height = 64;
+
     // Loads the cube values in the mesh data structure
-    //load_cube_mesh_data();
+    load_cube_mesh_data();
 
     // Loads the OBJ values to the mesh data structure
-    load_obj_file_data("./assets/f22.obj");
+    //load_obj_file_data("./assets/f22.obj");
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -90,6 +84,10 @@ void process_input(void) {
                 render_mode = RENDER_FILL_TRIANGLE;
             if(event.key.keysym.sym == SDLK_4)
                 render_mode = RENDER_FILL_TRIANGLE_WIRE;
+            if(event.key.keysym.sym == SDLK_5)
+                render_mode = RENDER_TEXTURED;
+            if(event.key.keysym.sym == SDLK_6)
+                render_mode = RENDER_TEXTURED_WIRE;
             if(event.key.keysym.sym == SDLK_c)
                 cull_method = CULL_BACKFACE;
             if(event.key.keysym.sym == SDLK_d)
@@ -97,17 +95,6 @@ void process_input(void) {
             break;
     }
 }
-
-////////////////////////////////////////////////////////////////////////////////
-// Function that receives a 3D vector and returns a projected 2D point
-////////////////////////////////////////////////////////////////////////////////
-// vec2_t project(vec3_t point) {
-//     vec2_t projected_point = {
-//         .x = (fov_factor * point.x) / point.z,
-//         .y = (fov_factor * point.y) / point.z
-//     };
-//     return projected_point;
-// }
 
 ///////////////////////////////////////////////////////////////////////////////
 // Update function frame by frame with a fixed time step
@@ -127,6 +114,8 @@ void update(void) {
     triangles_to_render = NULL;
 
     mesh.rotation.x += 0.01;    
+    mesh.rotation.y += 0.003;    
+    mesh.rotation.z += 0.008;    
     mesh.translation.z = 5.0;
 
     mat4_t scale_matrix = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.z);
@@ -161,19 +150,6 @@ void update(void) {
 
             transformed_vertex = mat4_mul_vec4(world_matrix, transformed_vertex);
             // USE A MATRIX
-            // transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
-            // transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
-            // transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
-            // transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
-            // transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
-
-            // transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
-            // transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
-            // transformed_vertex = vec3_rotate_z(transformed_vertex, mesh.rotation.z);
-
-            // Translate the vertex away from the camera
-            // transformed_vertex.z += 5;
-
             transformed_vertices[j] = transformed_vertex;
         }
 
@@ -237,6 +213,11 @@ void update(void) {
                 { projected_points[1].x, projected_points[1].y },
                 { projected_points[2].x, projected_points[2].y }
             },
+            .texcoords = {
+                {mesh_face.a_uv.u, mesh_face.a_uv.v},
+                {mesh_face.b_uv.u, mesh_face.b_uv.v},
+                {mesh_face.c_uv.u, mesh_face.c_uv.v}
+            },
             .color = triangle_color,
             .avg_depth = avg_depth
         };
@@ -291,7 +272,18 @@ void render(void) {
             );
         }
         
-        if(render_mode == RENDER_WIRE || render_mode == RENDER_WIRE_VERTEX || render_mode == RENDER_FILL_TRIANGLE_WIRE)
+        // Draw Textured Triangle
+        if(render_mode == RENDER_TEXTURED || render_mode == RENDER_TEXTURED_WIRE){
+            draw_textured_triangle(
+                triangle.points[0].x, triangle.points[0].y, triangle.texcoords[0].u, triangle.texcoords[0].v,// vertex A
+                triangle.points[1].x, triangle.points[1].y, triangle.texcoords[1].u, triangle.texcoords[1].v,// vertex B
+                triangle.points[2].x, triangle.points[2].y, triangle.texcoords[2].u, triangle.texcoords[2].v,// vertex C
+                mesh_texture
+            );
+        }
+
+
+        if(render_mode == RENDER_WIRE || render_mode == RENDER_WIRE_VERTEX || render_mode == RENDER_FILL_TRIANGLE_WIRE || render_mode == RENDER_TEXTURED_WIRE)
         {
             // Draw unfilled triangle
             draw_triangle(
